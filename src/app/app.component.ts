0ee120bc78f6fa5e498e56418e5fe2d287d30598
ShimkoIvan <googianat@gmail.com>
2020-08-19 14:46:07 +0300

    подключение шаблона html, css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/css/material-dashboard.css']
 })
 
 export class AppComponent {
-  title = 'my-app';
+  title = 'title';
 }
