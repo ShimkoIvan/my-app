074546ff3a20c27223d9cc094dcec1121e4b39d8
ShimkoIvan <googianat@gmail.com>
2020-09-04 21:36:24 +0300

    добавлены компонент и сервис categories(статичные), декомпозиция

---
+++ src/app/service/data-handler.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+
+//  providedIn: 'root' область доступа: 'начиная с корня'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataHandlerService {
+
+  constructor() {
+  }
+}
+
