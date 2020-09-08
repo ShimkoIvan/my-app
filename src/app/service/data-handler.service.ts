9c1f9dea352f81d5741d8c8e9142e498e9ceb845
ShimkoIvan <googianat@gmail.com>
2020-09-08 11:50:20 +0300

    добавлены отображение категорий в левом меню

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {Category} from '../model/Category';
+import {TestData} from '../data/TestData';
 
 //  providedIn: 'root' область доступа: 'начиная с корня'
 
@@ -7,8 +9,12 @@ import {Injectable} from '@angular/core';
 })
 
 export class DataHandlerService {
-
   constructor() {
+  }
+
+  // получает данные из массива
+  getCategories(): Category[] {
+    return TestData.categories;
   }
 }
 
